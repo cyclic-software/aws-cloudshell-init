8a3ea75db30aa03d087c31ad4b67614288d50ab6
Kam Lasater <kam@cyclic.sh>
2021-09-15 21:36:32 -0400

    poll-url.sh

---
+++ bin/poll-url.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+host=$1
+
+while true; do
+    curl --silent "https://${host}/" \
+    | jq -r --arg REGION "from $AWS_REGION\@" --arg DATESTR "$(date +%H:%M:%S)" '[$REGION, $DATESTR, .region, .message] | @tsv'
+    sleep 3;
+done;
